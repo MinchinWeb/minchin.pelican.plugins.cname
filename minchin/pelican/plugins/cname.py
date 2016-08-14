1b540d4c510c3cfda44484d0d471f955d0648332
MinchinWeb <w_minchin@hotmail.com>
2016-08-13 22:30:06 -0600

    Add original code
    https://github.com/getpelican/pelican-plugins/pull/566

---
+++ minchin/pelican/plugins/cname.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*- #
+"""
+CName
+=============
+
+This plugin add a *CNAME* file with contents based on ``SITEURL``.
+"""
+
+__version__ = '1.0.0'
+
+import os
+from pelican import signals
+
+def add_cname(p):
+    """ 
+    :param p: pelican instance
+    :return: None
+    """
+
+    cname_path = os.path.join(p.output_path, 'CNAME')
+    siteurl = p.settings.get('SITEURL', "127.0.0.1:8000")
+    siteurl = siteurl.replace("http://", "")
+    with open(cname_path, 'w') as cname_file:
+        cname_file.write(siteurl)    
+
+def register():
+    signals.finalized.connect(add_cname)
